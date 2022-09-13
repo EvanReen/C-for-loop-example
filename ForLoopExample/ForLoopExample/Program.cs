09921c76b1b12b89b018f6877b2802f1d5453fdd
Evan Reen <74310324+EvanReen@users.noreply.github.com>
2022-09-13 09:45:47 +0100

    Add files via upload

---
+++ ForLoopExample/ForLoopExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForLoopExample
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int myVal = 15;
+
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 8, 7, 6 };
+
+            Console.WriteLine();
+            Console.WriteLine("----- Printing myVal -----");
+
+            for (int i=0; i < myVal; i++)
+            {
+                Console.WriteLine("i is currently {0}", i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----- Printing array of nums -----");
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine("i is currently {0}", i);
+            }
+        }
+    }
+
+}
+
+            
